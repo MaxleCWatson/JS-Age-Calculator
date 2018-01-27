c029ce2d12f465cf04f9ac2d443223aef7060a64
Maxle Watson <maxlewatsonfitness@gmail.com>
2018-01-27 01:25:24 -0600

    Update age calculator.js

--- age calculator.js
+++ age calculator.js
@@ -1,4 +1,4 @@
-var age = prompt("How young are you?");
+var age = prompt("How many days have you been alive? Lets find out, how old are you?");
 var days = age * 365
 alert(age + " years is roughly " + days + " days! Congratulations!!");
 console.log("You've been alive for " + days + " days.");
