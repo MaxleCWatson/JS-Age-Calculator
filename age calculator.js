85eeb3863cd5f5a9e09fda152e4204a1175767fe
Maxle Watson <maxlewatsonfitness@gmail.com>
2018-01-27 01:24:14 -0600

    Add files via upload

---
+++ age calculator.js
@@ -0,0 +1,4 @@
+var age = prompt("How young are you?");
+var days = age * 365
+alert(age + " years is roughly " + days + " days! Congratulations!!");
+console.log("You've been alive for " + days + " days.");
